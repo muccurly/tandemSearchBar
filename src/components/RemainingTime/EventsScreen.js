fcabe858834cd9725b20f4f962b756f09d1e3bfa
muccurly <mirmiras04@mail.ru>
2021-01-27 13:00:55 +0600

    Remaining Time changed

--- src/components/RemainingTime/EventsScreen.js
+++ src/components/RemainingTime/EventsScreen.js
@@ -1,11 +1,11 @@
 import moment from 'moment';
 import React,{useState,useEffect} from 'react'
 import {View} from 'react-native'
-import RemaningTime from './RemainingTime'
+import RemainingTime from './RemainingTime'
 const timeOfEvent = Date.now()+((((60000*60)*24)*7)*5)
 const timeOfEvent2 = Date.now()+((((60000*60)*24)*7)*4)*16
 const timeOfEvent4 = Date.now()+(60000*60)*13
-const timeOfEvent5 = Date.now()+((60000*60))
+const timeOfEvent5 = Date.now()+((60000*32))
 const timeOfEvent3 = Date.now()+((80000))
 export default function EventsScreen(props){
     const [currentTime,setCurrentTime] = useState(moment());
@@ -20,11 +20,11 @@ export default function EventsScreen(props){
 
     return(
         <View>
-            <RemaningTime timestamp = {timeOfEvent}  currentstamp = {currentTime}/>
-            <RemaningTime timestamp = {timeOfEvent2}  currentstamp = {currentTime}/>
-            <RemaningTime timestamp = {timeOfEvent3}  currentstamp = {currentTime}/>
-            <RemaningTime timestamp = {timeOfEvent4}  currentstamp = {currentTime}/>
-            <RemaningTime timestamp = {timeOfEvent5}  currentstamp = {currentTime}/>
+            <RemainingTime timestamp = {timeOfEvent}  currentstamp = {currentTime}/>
+            <RemainingTime timestamp = {timeOfEvent2}  currentstamp = {currentTime}/>
+            <RemainingTime timestamp = {timeOfEvent3}  currentstamp = {currentTime}/>
+            <RemainingTime timestamp = {timeOfEvent4}  currentstamp = {currentTime}/>
+            <RemainingTime timestamp = {timeOfEvent5}  currentstamp = {currentTime}/>
         </View>
     )
 }
